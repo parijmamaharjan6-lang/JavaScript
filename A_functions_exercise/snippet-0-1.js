c4db0a9295f452035c6fbd49f3aad31fd90e88eb
Parijma Mahrjan <parijmamaharjan@gmail.com>
2026-04-21 17:36:36 +0530

    01

---
+++ A_functions_exercise/snippet-0-1.js
@@ -0,0 +1,16 @@
+// snippet 0-1
+let greet = function () {
+  console.log("hey"); //hey
+  console.log("programmers"); // programers
+};
+
+let whistle = function () {
+  console.log("doot");// doot
+};
+
+console.log("first");//first
+console.log("second");//second
+greet();//hey programers
+console.log("third");//third
+console.log("fourth");//fourth
+whistle();//doot
