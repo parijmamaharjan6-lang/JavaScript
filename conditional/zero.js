64864e4d2fcbd24e5cac93f19744a841041ef13b
Parijma Mahrjan <parijmamaharjan@gmail.com>
2026-04-21 17:27:43 +0530

    010

--- conditional/zero.js
+++ conditional/zero.js
@@ -44,3 +44,18 @@ if (word[0] === "d") {
 } else {
   console.log("nah");//nah
 }
+
+// snippet 0-5
+let sentence = "roger that";
+
+if (sentence[sentence.length - 1] === "t") {
+  console.log("ends in t");
+} else {
+  console.log("does not end in t");
+}
+
+if (sentence.length <= 4) {
+  console.log("short");
+} else {
+  console.log("long");
+}
