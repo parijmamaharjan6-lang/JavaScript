97a58b741ef6ab23d69fd450ffadc1932383d6e2
Parijma Mahrjan <parijmamaharjan@gmail.com>
2026-04-21 17:31:45 +0530

    01

--- conditional/one.js
+++ conditional/one.js
@@ -10,3 +10,35 @@ if (qty > 30 && qty % 5 === 4) {
 if (qty > 0) {
   console.log("pos");//pos
 }
+
+// snippet 1-1
+let a = "celery";
+let b = "SQUASH";
+
+if (a === a.toUpperCase()) {
+  console.log("alpha");
+}
+
+if (b === b.toUpperCase()) {
+  console.log("beta");
+}
+
+// snippet 1-2
+let number = 9;
+
+if (number > 4) {
+  console.log("ding");
+} else if (number % 3 === 0) {
+  console.log("dong");
+}
+
+// snippet 1-3
+let z = 12;
+
+if (z > 10) {
+  console.log("vroom");
+}
+
+if (z % 3 === 0) {
+  console.log("skrrt");
+}
