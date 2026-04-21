a09daa9982cb5d4b94e05716f4959605339469be
Parijma Mahrjan <parijmamaharjan@gmail.com>
2026-04-21 17:29:47 +0530

    01

---
+++ conditional/one.js
@@ -0,0 +1,12 @@
+// snippet 1-0
+let qty = 38;
+
+if (qty > 30 && qty % 5 === 4) {
+  console.log("swish");
+} else {
+  console.log("swoosh");// swoosh
+}
+
+if (qty > 0) {
+  console.log("pos");//pos
+}
