61b113cf49de25a8d3a6a7fcc7af462d347fbec1
Parijma Mahrjan <parijmamaharjan@gmail.com>
2026-04-21 17:45:27 +0530

    04

---
+++ functions/A_functions_exercise/snippet-0-4.js
@@ -0,0 +1,11 @@
+// snippet 0-4
+let exclaim = function (str) {
+  let capitalized = str.toUpperCase();
+  return capitalized + "!!";
+};
+
+let result = exclaim("potato");
+console.log(result);//POTATO
+console.log(result.length);//8
+console.log(result[0]);//P
+console.log(result[result.length - 1]);//!
