1b677aa88e608b4ad456b08aa11d8e9c8ad3c60e
Parijma Mahrjan <parijmamaharjan@gmail.com>
2026-04-26 16:33:07 +0545

    fun0

---
+++ functions/A_functions_exercise/is-long.js
@@ -0,0 +1,16 @@
+// Write a function `isLong` that accepts a string as an argument. The function should return a boolean
+// indicating whether or not the string is longer than 5 characters
+
+let isLong = function(s){
+    if (s.length > 5){
+        return true;
+    }
+    else return false;
+}
+
+console.log(isLong("pie")); // false
+console.log(isLong("kite")); // false
+console.log(isLong("kitty")); // false
+console.log(isLong("telescope")); // true
+console.log(isLong("thermometer")); // true
+console.log(isLong("restaurant")); // true
