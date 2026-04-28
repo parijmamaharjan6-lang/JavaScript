23493f03b2b6bf5c5cec831bc145eae850f13133
Parijma Mahrjan <parijmamaharjan@gmail.com>
2026-04-28 18:02:51 +0545

    fun01

---
+++ functions/C_functions_exercise/case-changer.js
@@ -0,0 +1,17 @@
+// Write a function `caseChange` that accepts a string and a boolean as arguments. The function should
+// return the uppercase version of the string if the boolean is true. The function should return the
+// lowercase version of the string if the boolean is false.
+
+const caseChange = (s, b)=>{
+    if(b ){
+        return s.toUpperCase();
+    }else{
+        return s.toLowerCase();
+    }
+}
+
+
+console.log(caseChange("Super", true)); // 'SUPER'
+console.log(caseChange("Super", false)); // 'super'
+console.log(caseChange("tAmBourine", true)); // 'TAMBOURINE'
+console.log(caseChange("tAmBourine", false)); // 'tambourine'
