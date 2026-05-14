6629f0bb60d11def4c29dd8fed4307ec559a572c
Parijma Mahrjan <parijmamaharjan@gmail.com>
2026-05-14 18:20:32 +0545

    nest3

--- 05_nesting/C_more_problems/shorten-long-words.js
+++ 05_nesting/C_more_problems/shorten-long-words.js
@@ -2,24 +2,31 @@
 // should return the same sentence where words longer than 4 characters have their vowels removed.
 
 function shortenLongWords(sentence) {
-  return sentence
-    .split(" ")
-    .map((word) => {
-      // Separate punctuation from the word
-      const match = word.match(/^([a-zA-Z]+)([^a-zA-Z]*)$/);
-
-      if (!match) return word;
-
-      let [_, text, punctuation] = match;
-
-      if (text.length > 4) {
-        text = text.replace(/[aeiou]/gi, "");
-      }
-
-      return text + punctuation;
-    })
-    .join(" ");
+  let result = []
+  let array = sentence.split(" ")
+  for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    if (element.length > 4) {
+      let answer = removeVowels(element)      
+      result.push(answer)
+    }else{
+      result.push(element)
+    }
+  }
+  return result.join(" ")
 }
+function removeVowels(string) {
+  let answer = "";
+  let vowels = "aeiou";
+  for (let i = 0; i < string.length; i++) {
+    const element = string[i];
+    if (!vowels.includes(element)) {
+      answer += element;
+    }
+  }
+  return answer;
+}
+
 
 console.log(shortenLongWords("they are very noble people")); // 'they are very nbl ppl'
 console.log(shortenLongWords("stick with it")); // 'stck with it'
